21d4e30638eab37e20d5d57251651b33197ea282
Yamada <57486435+alphadigital1@users.noreply.github.com>
2019-11-11 14:14:58 -0300

    Add files via upload

--- app/share/config/config.php
+++ app/share/config/config.php
@@ -1,1 +1,30 @@
 
+<?php
+
+	// Database Common
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'ialbooks');
+	define('DB_USER', 'ialbooks');
+	define('DB_PASS', 'ialbooks2019@');
+	define('DB_CHARSET', 'utf8');
+
+	// E-mail configuration
+	define('EMAIL_HOST', 'smtp.gmail.com');
+	define('EMAIL_PORT', 587);
+	define('EMAIL_USER', '');
+	define('EMAIL_PASS', '');
+	define('EMAIL_FROM_NAME', '');
+	define('EMAIL_FROM_EMAIL', 'contato@alphalumen.org.br');
+	define('EMAIL_SMTPSECURE', 'tls');
+	define('EMAIL_SMTPDEBUG', 0); //0 produção, 2 debug
+	define('EMAIL_SMTPAUTH', true);
+	define('EMAIL_MAILER', 'smtp');
+	define('EMAIL_CHARSET', 'utf-8');
+	define('EMAIL_ISHTML', true);
+
+	// ENDEREÇO DO SERVIDOR
+	define('HOSTNAME', 'localhost');
+
+	define('WEBAPP_NAME', 'Clube do Livro');
+
+?>
