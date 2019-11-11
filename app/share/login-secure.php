aa4661141be23bf093d751b4ff1c4499b54ebb5a
alphadigital1 <tecnologia@alphalumen.org.br>
2019-11-11 16:43:20 -0300

    Update login-secure.php

--- app/share/login-secure.php
+++ app/share/login-secure.php
@@ -9,4 +9,28 @@
         header("Location: ../login.php");
     }
 
+    $username = $_SESSION['ulogin'];
+    $password = $_SESSION['upaswd'];
+    $user_id = $_SESSION['user_id'];
+    $profile_id = $_SESSION['profile_id'];
+    $email = $_SESSION['email'];
+    $contact_id = $_SESSION['contact_id'];
+    $fullname = $_SESSION['fullname'];
+    $name = $_SESSION['name'];
+
+    $acesso   =    User::getInstance();
+    $contato  = Contact::getInstance();
+    $endereco = Address::getInstance();
+
+    $acesso->setSaveId($contact_id);
+    $contato->setSaveId($contact_id);
+    $endereco->setSaveId($contact_id);
+
+    $acesso->setLogin($username);
+    $acesso->setPassword($password);
+
+    if(!$acesso->passwordCheck() )
+        header("Location: login.php");
+
+
 ?>
