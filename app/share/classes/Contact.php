dacb88917b97100e538da951222989aef1415252
alphadigital1 <tecnologia@alphalumen.org.br>
2019-11-14 15:46:15 -0300

    Update Contact.php

--- app/share/classes/Contact.php
+++ app/share/classes/Contact.php
@@ -33,13 +33,15 @@ class Contact {
 	protected $cpf='';
 	protected $marital_status='';
 	protected $deceased='N';
+
+	protected $address_number='';
+	protected $address_complement='';
+
 	protected $active='Y';
 	protected $created;
 	protected $createdby;
 	protected $modified;
 	protected $modifiedby;
-	protected $house_number;
-	protected $address_complement;
 
 	private function __construct(){   
 		$this->pdo = Connection::getInstance();  
@@ -138,6 +140,32 @@ class Contact {
 	public function getCPF() {
 		return $this->cpf;
 	}
+	public function setMaritalStatus( $var ) {
+		$this->marital_status = $var;
+	}
+	public function getMaritalStatus() {
+		return $this->marital_status;
+	}
+	public function setDeceased( $var ) {
+		$this->deceased = $var;
+	}
+	public function getDeceased() {
+		return $this->deceased;
+	}
+
+	public function setAddressNumber( $var ) {
+		$this->address_number = $var;
+	}
+	public function getAddressNumber() {
+		return $this->address_number;
+	}
+	public function setAddressComplement( $var ) {
+		$this->address_complement = $var;
+	}
+	public function getAddressComplement() {
+		return $this->address_complement;
+	}
+
 	public function setActive( $var ) {
 		$this->active = $var;
 	}
@@ -168,18 +196,6 @@ class Contact {
 	public function getModifiedBy() {
 		return $this->modifiedby;
 	}
-	public function setHouseNumber( $var ) {
-		$this->house_number = $var;
-	}
-	public function getHouseNumber() {
-		return $this->house_number;
-	}
-	public function setAddressComplement( $var ) {
-		$this->address_complement = $var;
-	}
-	public function getAddressComplement() {
-		return $this->address_complement;
-	}
 
 	
 	public function insertQuick()
@@ -207,7 +223,7 @@ class Contact {
 	{
 		$this->crud->setTablename($this->table);
 
-		$array = array('user_id'=> $this->user_id, 'address_id'=> $this->address_id, 'name'=> $this->name, 'email'=> $this->email, 'gender'=> $this->gender, 'birthdate'=> $this->birthdate, 'phone'=> $this->phone, 'mobile'=> $this->mobile, 'rg'=> $this->rg, 'rg_emissor'=> $this->rg_emissor, 'cpf'=> $this->cpf, 'active'=> $this->active, 'createdby'=> $this->save_id, 'house_number'=> $this->house_number, 'address_complement'=> $this->address_complement);
+		$array = array('user_id'=> $this->user_id, 'address_id'=> $this->address_id, 'name'=> $this->name, 'email'=> $this->email, 'gender'=> $this->gender, 'birthdate'=> $this->birthdate, 'phone'=> $this->phone, 'mobile'=> $this->mobile, 'rg'=> $this->rg, 'rg_emissor'=> $this->rg_emissor, 'cpf'=> $this->cpf, 'marital_status'=> $this->marital_status, 'deceased'=>$this->deceased, 'address_number'=> $this->address_number, 'address_complement'=> $this->address_complement, 'active'=> $this->active, 'createdby'=> $this->save_id, );
 		
 		$id = array();
         $result = $this->crud->insert($array);
@@ -228,7 +244,7 @@ class Contact {
 	{
 		$this->crud->setTablename($this->table);
 
-		$array = array('user_id'=> $this->user_id, 'address_id'=> $this->address_id, 'name'=> $this->name, 'email'=> $this->email, 'gender'=> $this->gender, 'birthdate'=> $this->birthdate, 'phone'=> $this->phone, 'mobile'=> $this->mobile, 'rg'=> $this->rg, 'rg_emissor'=> $this->rg_emissor, 'cpf'=> $this->cpf, 'active'=> $this->active, 'modifiedby'=> $this->save_id, 'house_number'=> $this->house_number, 'address_complement'=> $this->address_complement);
+		$array = array('user_id'=> $this->user_id, 'address_id'=> $this->address_id, 'name'=> $this->name, 'email'=> $this->email, 'gender'=> $this->gender, 'birthdate'=> $this->birthdate, 'phone'=> $this->phone, 'mobile'=> $this->mobile, 'rg'=> $this->rg, 'rg_emissor'=> $this->rg_emissor, 'cpf'=> $this->cpf, 'marital_status'=> $this->marital_status, 'address_number'=> $this->address_number, 'address_complement'=> $this->address_complement, 'active'=> $this->active, 'modifiedby'=> $this->save_id, );
 		  
 		$arrayCond = array('id=' => $this->id);
 	  
