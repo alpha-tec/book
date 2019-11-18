6c1249529f4db98342418aaf7680b134d5138d04
alphadigital1 <tecnologia@alphalumen.org.br>
2019-11-18 17:45:33 -0300

    Update profiles.php

--- app/setup/profiles.php
+++ app/setup/profiles.php
@@ -18,10 +18,30 @@
 </div>
 
 <form class="form form-validate"  method="post" action="">
-    <section class="forms" style="width: 100%;">
-        <div class="container-fluid " style="width: 100%;">
+    <section class="forms" >
+        <div class="container-fluid " >
 
-        <div class="row mt-1">
+            <div class="row mt-1">
+
+                <div class="form-group col-sm-3 col-lg-2">
+                    <label class="form-control-label "><strong>Status</strong></label>
+                    <select name="inStatus" data-msg="Informe o status no processo" class="form-control form-control-sm " >
+                        <option value="Y" >Ativo</option>
+                        <option value="N" >Inativo</option>
+                    </select>
+                </div>
+
+                <div class="form-group col-sm-2 col-lg-1">
+                    <button class="btn btn-sm btn-info mt-3" name="pesquisar" value="10" type="submit" data-toggle="tooltip" data-placement="top" title="Pesquisar"><i class="fa fa-search"></i> Pesquisar</button>
+                </div>
+
+                <div class="form-group col-sm-2 col-lg-2">
+                    <button class="btn btn-sm btn-success ml-4 mt-3" name="criar" value="10" type="submit" data-toggle="tooltip" data-placement="top" title="Criar novo item"><i class="fas fa-plus"></i> Criar</button>
+                </div>
+
+            </div>
+            <hr class="my-2">
+
 
             <table id="example" class="table table-sm table-striped table-bordered" style="width:100%" > 
                 <thead class="thead-dark">
@@ -49,7 +69,12 @@
                             echo ' </td> ';
 
                             echo ' <td class="text-center"> ';
-                            echo $value->id;
+
+                            echo ' <span title="Editar Perfil" data-toggle="tooltip" data-placement="top"><button type="button" class="btn btn-sm btn-info " aria-hidden="true" data-toggle="modal" data-target="#editMenu" ><i class="fas fa-edit"></i> </button></span> ';
+
+                            echo '<button type="button" class="btn btn-sm btn-danger ml-2 " data-toggle="tooltip" data-placement="top" title="Desativar este perfil"><i class="fas fa-times"></i> </button>';
+
+                            //echo $value->id;
                             echo ' </td> ';
 
                             echo ' <td class="text-center"> ';
@@ -79,12 +104,10 @@
 
                             echo ' </tr> ';
                         }
-                        ?>
-                    </tbody>
-                </table>
+                    ?>
+                </tbody>
+            </table>
                      
-            </div>
-
         </div>
     </section>
 </form>
